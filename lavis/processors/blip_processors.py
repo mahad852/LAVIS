06d1bacb988dad5a764622eeea0f7e2db2658ecb
Mahad <mahad852@gmail.com>
2024-03-01 02:48:24 -0500

    added processor and config for videos for blip2 model

--- lavis/processors/blip_processors.py
+++ lavis/processors/blip_processors.py
@@ -13,8 +13,23 @@ from lavis.processors.randaugment import RandomAugment
 from omegaconf import OmegaConf
 from torchvision import transforms
 from torchvision.transforms.functional import InterpolationMode
+from lavis.processors import transforms_video
+import torch
+from lavis.datasets.data_utils import load_video
+from lavis.processors.alpro_processors import ToTHWC, ToUint8
 
+MAX_INT = registry.get("MAX_INT")
 
+class VideoMean(object):
+    def __init__(self):
+        pass
+
+    def __call__(self, tensor: torch.Tensor):
+        return tensor.mean(dim = 0)
+
+    def __repr__(self):
+        return self.__class__.__name__
+    
 class BlipImageBaseProcessor(BaseProcessor):
     def __init__(self, mean=None, std=None):
         if mean is None:
@@ -23,6 +38,16 @@ class BlipImageBaseProcessor(BaseProcessor):
             std = (0.26862954, 0.26130258, 0.27577711)
 
         self.normalize = transforms.Normalize(mean, std)
+
+class BlipVideoBaseProcessor(BaseProcessor):
+    def __init__(self, mean=None, std=None, n_frms=MAX_INT):
+        if mean is None:
+            mean = (0.48145466, 0.4578275, 0.40821073)
+        if std is None:
+            std = (0.26862954, 0.26130258, 0.27577711)
+
+        self.normalize = transforms_video.NormalizeVideo(mean, std)
+        self.n_frms = n_frms
 
 
 @registry.register_processor("blip_caption")
@@ -192,6 +217,50 @@ class BlipImageEvalProcessor(BlipImageBaseProcessor):
         std = cfg.get("std", None)
 
         return cls(image_size=image_size, mean=mean, std=std)
+    
+@registry.register_processor("blip_video_eval")
+class BlipVideoEvalProcessor(BlipVideoBaseProcessor):
+    def __init__(self, image_size=384, mean=None, std=None, n_frms=MAX_INT):
+        super().__init__(mean=mean, std=std, n_frms=n_frms)
+        self.image_size = image_size
+
+        self.transform = transforms.Compose(
+            [
+                ToTHWC(),  # T, H, W, C
+                VideoMean(), # H, W, C
+                # ToUint8(),  # H, W, C
+                transforms.Resize(
+                    (image_size, image_size), interpolation=InterpolationMode.BICUBIC
+                ),
+                transforms.ToTensor(),
+                self.normalize
+            ]
+        )
+
+    def __call__(self, vpath):
+        clip = load_video(
+            video_path=vpath,
+            n_frms=self.n_frms,
+            height=self.image_size,
+            width=self.image_size,
+        )
+
+        return self.transform(clip)
+
+
+    @classmethod
+    def from_config(cls, cfg=None):
+        if cfg is None:
+            cfg = OmegaConf.create()
+
+        image_size = cfg.get("image_size", 384)
+
+        mean = cfg.get("mean", None)
+        std = cfg.get("std", None)
+
+        n_frms = cfg.get("n_frms", MAX_INT)
+
+        return cls(image_size=image_size, mean=mean, std=std, n_frms=n_frms)
 
 
 @registry.register_processor("blip2_image_train")
