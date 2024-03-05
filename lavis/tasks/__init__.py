21c2709e77d7c1683fad4229a62ea555a2f4580f
Mahad <mahad852@gmail.com>
2024-03-05 15:42:43 -0500

    trying to fix task retrieval error

--- lavis/tasks/__init__.py
+++ lavis/tasks/__init__.py
@@ -13,6 +13,7 @@ from lavis.tasks.multimodal_classification import (
     MultimodalClassificationTask,
 )
 from lavis.tasks.retrieval import RetrievalTask
+from lavis.tasks.wgv_retrieval import WGVRetrievalTask
 from lavis.tasks.vqa import VQATask, GQATask, AOKVQATask
 from lavis.tasks.vqa_reading_comprehension import VQARCTask, GQARCTask
 from lavis.tasks.dialogue import DialogueTask
