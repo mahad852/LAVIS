0d7e9057f8416a32a1312123d3a539f0d7b9ff6d
Mahad <mahad852@gmail.com>
2024-04-13 18:11:46 -0400

    added slurm files for stage1 training of gvt

--- lavis/tasks/base_task.py
+++ lavis/tasks/base_task.py
@@ -113,7 +113,7 @@ class BaseTask:
     ):
         return self._train_inner_loop(
             epoch=epoch,
-            iters_per_epoch=200,
+            iters_per_epoch=len(data_loader),
             model=model,
             data_loader=data_loader,
             optimizer=optimizer,
