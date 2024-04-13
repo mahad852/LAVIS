feb62902c9ad2ad8a2af3b2742a149272259f9f6
Mahad <mahad852@gmail.com>
2024-04-13 01:35:15 -0400

    passing return_all_features=True to gvt vit

--- lavis/models/blip2_models/blip2_gvt.py
+++ lavis/models/blip2_models/blip2_gvt.py
@@ -49,10 +49,10 @@ class Blip2GVT(Blip2OPT):
     def forward(self, samples):
         image = samples["image"]
         with self.maybe_autocast():
-            image_embeds = self.visual_encoder_gvt.forward_features(image)
+            image_embeds = self.visual_encoder_gvt.forward_features(image, return_all_features=True)
             print("vit response shape:", image_embeds.shape)
             self.ln_vision(self.reduction_layer(image_embeds))
-            
+
         image_atts = torch.ones(image_embeds.size()[:-1], dtype=torch.long).to(
             image.device
         )
@@ -137,7 +137,7 @@ class Blip2GVT(Blip2OPT):
         """
         image = samples["image"]
         with self.maybe_autocast():
-            image_embeds = self.ln_vision(self.reduction_layer(self.visual_encoder_gvt.forward_features(image)))
+            image_embeds = self.ln_vision(self.reduction_layer(self.visual_encoder_gvt.forward_features(image, return_all_features=True)))
             image_atts = torch.ones(image_embeds.size()[:-1], dtype=torch.long).to(
                 image.device
             )
@@ -212,7 +212,7 @@ class Blip2GVT(Blip2OPT):
     ):
         image = samples["image"]
         with self.maybe_autocast():
-            image_embeds = self.ln_vision(self.reduction_layer(self.visual_encoder_gvt.forward_features(image)))
+            image_embeds = self.ln_vision(self.reduction_layer(self.visual_encoder_gvt.forward_features(image, return_all_features=True)))
             image_atts = torch.ones(image_embeds.size()[:-1], dtype=torch.long).to(
                 image.device
             )
