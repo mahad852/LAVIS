f2ffdb185ea6aec9b623b156909aeaf427c7c8a1
Mahad <mahad852@gmail.com>
2024-04-16 22:57:57 -0400

    making prompts consistent for coco instruct

--- lavis/datasets/datasets/coco_caption_datasets.py
+++ lavis/datasets/datasets/coco_caption_datasets.py
@@ -55,7 +55,7 @@ class COCOCapInstructDataset(CaptionDataset):
 
         return {
             "image": image,
-            "text_input": "a photo of",
+            "text_input": "a photo of ",
             "text_output" : caption,
             "image_id": self.img_ids[ann["image_id"]],
         }
