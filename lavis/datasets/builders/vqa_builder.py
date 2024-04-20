34f5b974fad7302d1256fd4e58a52a8f8bccfe97
Mahad <mahad852@gmail.com>
2024-04-20 12:33:17 -0400

    fixing incorrect config for gvt vicuna in mmvp

--- lavis/datasets/builders/vqa_builder.py
+++ lavis/datasets/builders/vqa_builder.py
@@ -24,6 +24,7 @@ class MMVPVQABuilder(BaseDatasetBuilder):
 
     DATASET_CONFIG_DICT = {
         "default": "configs/datasets/mvpp/defaults_vqa.yaml",
+        "eval" : "default": "configs/datasets/mvpp/defaults_vqa.yaml"
     }
 
 
