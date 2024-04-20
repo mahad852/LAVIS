cda0d77fc961c831519c35b150f15d952264d07a
Mahad <mahad852@gmail.com>
2024-04-20 12:57:22 -0400

    fixing weight loading for vicuna_gvt

--- lavis/models/blip2_models/blip2_gvt_vicuna.py
+++ lavis/models/blip2_models/blip2_gvt_vicuna.py
@@ -169,13 +169,63 @@ class Blip2GVTVicuna(Blip2VicunaInstruct):
         return {"loss": loss}
     
     def load_from_pretrained(self, url_or_filename):
-        super.__init__(url_or_filename)
+        super().load_from_pretrained(url_or_filename)
         
         if os.path.exists("lavis/output/BLIP2_GVT/Pretrain_stage1_vicuna/20240417072/checkpoint_best.pth"):
             weights = torch.load("lavis/output/BLIP2_GVT/Pretrain_stage1_vicuna/20240417072/checkpoint_best.pth")
             self.load_state_dict(weights['model']) 
 
             print("LOADED FINETUED WEIGHTS")
+
+    
+    @classmethod
+    def from_config(cls, cfg):
+        vit_model = cfg.get("vit_model", "eva_clip_g")
+        img_size = cfg.get("image_size")
+        num_query_token = cfg.get("num_query_token")
+        llm_model = cfg.get("llm_model")
+
+        drop_path_rate = cfg.get("drop_path_rate", 0)
+        use_grad_checkpoint = cfg.get("use_grad_checkpoint", False)
+        vit_precision = cfg.get("vit_precision", "fp16")
+        freeze_vit = cfg.get("freeze_vit", True)
+
+        prompt = cfg.get("prompt", "")
+        max_txt_len = cfg.get("max_txt_len", 128)
+        max_output_txt_len = cfg.get("max_output_txt_len", 256)
+
+        apply_lemmatizer = cfg.get("apply_lemmatizer", False)
+
+        qformer_text_input = cfg.get("qformer_text_input", True)
+
+        load_gvt_weights = cfg.get("load_gvt_weights", True)
+
+        model = cls(
+            vit_model=vit_model,
+            img_size=img_size,
+            drop_path_rate=drop_path_rate,
+            use_grad_checkpoint=use_grad_checkpoint,
+            vit_precision=vit_precision,
+            freeze_vit=freeze_vit,
+            num_query_token=num_query_token,
+            llm_model=llm_model,
+            prompt=prompt,
+            max_txt_len=max_txt_len,
+            max_output_txt_len=max_output_txt_len,
+            apply_lemmatizer=apply_lemmatizer,
+            qformer_text_input=qformer_text_input,
+            load_gvt_weights=load_gvt_weights
+        )
+
+        # if qformer_text_input:
+        #     # Hard-coded to load from BLIP-2 stage-1 pre-trained model (not ideal)
+        #     model.load_from_pretrained(
+        #         url_or_filename="https://storage.googleapis.com/sfr-vision-language-research/LAVIS/models/BLIP2/blip2_pretrained.pth"
+        #     )
+
+        model.load_checkpoint_from_config(cfg)
+
+        return model
 
     @torch.no_grad()
     def generate(
