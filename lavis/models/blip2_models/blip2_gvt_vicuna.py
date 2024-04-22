9b35e370f87b6b277d9bd393678275bd389b7b1d
Mahad <mahad852@gmail.com>
2024-04-22 07:11:01 -0400

    loading finetune_coco weights in gvt_vicuna

--- lavis/models/blip2_models/blip2_gvt_vicuna.py
+++ lavis/models/blip2_models/blip2_gvt_vicuna.py
@@ -182,8 +182,12 @@ class Blip2GVTVicuna(Blip2VicunaInstruct):
     def load_from_pretrained(self, url_or_filename):
         super().load_from_pretrained(url_or_filename)
         
-        if os.path.exists("lavis/output/BLIP2_GVT/Pretrain_stage1_vicuna/20240421174/checkpoint_best.pth"):
-            weights = torch.load("lavis/output/BLIP2_GVT/Pretrain_stage1_vicuna/20240421174/checkpoint_best.pth")
+        # if os.path.exists("lavis/output/BLIP2_GVT/Pretrain_stage1_vicuna/20240421174/checkpoint_best.pth"):
+        #     weights = torch.load("lavis/output/BLIP2_GVT/Pretrain_stage1_vicuna/20240421174/checkpoint_best.pth")
+        #     self.load_state_dict(weights['model'], strict=False) 
+
+        if os.path.exists("lavis/output/BLIP2_GVT/Pretrain_stage1/finetune_opt6.7b_coco/checkpoint_best.pth"):
+            weights = torch.load("lavis/output/BLIP2_GVT/Pretrain_stage1/finetune_opt6.7b_coco/checkpoint_best.pth")
             self.load_state_dict(weights['model'], strict=False) 
         
         if os.path.exists("lavis/output/BLIP2/coco_finetuned/blip2_finetune_coco.pth"):
