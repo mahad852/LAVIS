465dc228778b61268545be38e00a61ca385c7ba0
Mahad <mahad852@gmail.com>
2024-04-22 13:48:55 -0400

    reverting vqa subset changes

--- lavis/tasks/base_task.py
+++ lavis/tasks/base_task.py
@@ -87,17 +87,12 @@ class BaseTask:
         print_freq = 10
 
         results = []
-        counter = 0
 
         for samples in metric_logger.log_every(data_loader, print_freq, header):
             samples = prepare_sample(samples, cuda_enabled=cuda_enabled)
 
             eval_output = self.valid_step(model=model, samples=samples)
             results.extend(eval_output)
-
-            counter += 1
-            if counter >= 10:
-                break
 
         if is_dist_avail_and_initialized():
             dist.barrier()
